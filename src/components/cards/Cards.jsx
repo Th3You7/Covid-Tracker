e813b740572043c44d7827d870d85d18762ea112
Th3You7 <elbasiriy@gmail.com>
2020-11-25 05:26:25 +0100

    adding loading, daily numbers

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,38 +1,65 @@
 import React from "react";
-import { Grid, Card, CardContent, Typography, Box } from "@material-ui/core";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  Divider,
+} from "@material-ui/core";
 import {
   LocalHospital,
   InsertEmoticon,
   Hotel,
   EventBusy,
+  TrendingUp,
 } from "@material-ui/icons";
 
 import classes from "./Cards.module.css";
 
-const Cards = ({ details: { confirmed, recovered, deaths, treated } }) => {
+const Cards = ({
+  details: {
+    confirmed,
+    recovered,
+    deaths,
+    treated,
+    new_confirmed,
+    new_recovered,
+    new_deaths,
+    new_treated,
+  },
+}) => {
   const cardsDetail = [
     {
       case: "Infected",
       number: confirmed,
+      dailyNum: new_confirmed,
       style: classes.infected,
+      iconColor: "red",
       icon: <LocalHospital style={{ color: "red", fontSize: 40 }} />,
     },
     {
       case: "Deaths",
       number: deaths,
+      dailyNum: new_deaths,
       style: classes.deaths,
+      iconColor: "red",
       icon: <EventBusy style={{ color: "red", fontSize: 40 }} />,
     },
     {
       case: "Active",
       number: treated,
+      dailyNum: new_treated,
       style: classes.active,
+      iconColor: "#ffa800",
       icon: <Hotel style={{ color: "#ffa800", fontSize: 40 }} />,
     },
     {
       case: "Recovered",
       number: recovered,
+      dailyNum: new_recovered,
       style: classes.recovered,
+      iconColor: "green",
       icon: <InsertEmoticon style={{ color: "green", fontSize: 40 }} />,
     },
   ];
@@ -69,6 +96,24 @@ const Cards = ({ details: { confirmed, recovered, deaths, treated } }) => {
               </Box>
               {item.icon}
             </CardContent>
+            <Divider />
+            {/*
+            /the little box that show the daily number 
+            */}
+            <Box style={{ padding: 16 }} className={item.style}>
+              <div
+                style={{
+                  backgroundColor: item.iconColor,
+                  color: "white",
+                }}
+                className={classes.dailyNumContainer}
+              >
+                <TrendingUp style={{ fontSize: 16, fontWeight: "bold" }} />
+                <span style={{ fontSize: 14, fontWeight: "bold" }}>
+                  {item.dailyNum}
+                </span>
+              </div>
+            </Box>
           </Grid>
         ))}
       </Grid>
--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import img from "../../imgs/coronavirus1.png";
+import classes from "./loading.module.css";
 const Loading = () => {
-  return <div>Loading....</div>;
+  return (
+    <div className={classes.container}>
+      <motion.img
+        src={img}
+        alt="coronavirus"
+        animate={{
+          scale: [1, 1.4, 1.4, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          loop: Infinity,
+          repeatDelay: 1,
+        }}
+      />
+    </div>
+  );
 };
 
 export default Loading;
